856bf47a60146507ec3f097f1924caf40c6fdbad
fahijrahaviqa <fahijrah23ti@mahasiswa.pcr.ac.id>
2025-01-10 05:44:01 +0700

    twenty-third commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -126,9 +126,9 @@
                 <div class="row g-5">
                     <div class="col-lg-3 col-md-6">
                         <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Company</h4>
-                        <a class="btn btn-link" href="">About Us</a>
-                        <a class="btn btn-link" href="">Contact Us</a>
-                        <a class="btn btn-link" href="">Reservation</a>
+                        <a class="btn btn-link" href="">Tentang Kami</a>
+                        <a class="btn btn-link" href="">Hubungi Kami</a>
+                        <a class="btn btn-link" href="">Reservasi</a>
                         <a class="btn btn-link" href="">Privacy Policy</a>
                         <a class="btn btn-link" href="">Terms & Condition</a>
                     </div>
